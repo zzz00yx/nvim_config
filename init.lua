5e69b8e2042e2d484e9a1dec80ac8386cbf91eaa
zzz00yx <zzz00yx@gmail.com>
2024-12-12 23:23:34 +0800

    Init

---
+++ init.lua
@@ -0,0 +1,72 @@
+
+vim.cmd("source ~/.vimrc")
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    { "neovim/nvim-lspconfig" },
+    { "folke/tokyonight.nvim", lazy = false },
+    {
+      "hrsh7th/nvim-cmp",
+      -- load cmp on InsertEnter
+      event = "InsertEnter",
+      -- these dependencies will only be loaded when cmp loads
+      -- dependencies are always lazy-loaded unless specified otherwise
+      dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+      },
+    }
+    -- add your plugins here
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = false },
+})
+
+require("tokyonight").setup({ style = "storm", transparent = true })
+vim.cmd([[colorscheme tokyonight]])
+
+require("nvim-treesitter.configs").setup({
+  ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query" },
+  highlight = { enable = true, additional_vim_regex_highlighting = false, }
+})
+
+local on_attach = function(_, bufnr)
+  local opts = { buffer = bufnr, noremap = true, silent = true }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.format, opts)
+end
+require("lspconfig").pyright.setup{ on_attach = on_attach, filetypes = { "python" }, }
+require("lspconfig").clangd.setup{ on_attach = on_attach, filetypes = { "c", "cpp" } }
+
